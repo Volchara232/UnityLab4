b4e34cb22c98729262a6b6809f700e193047b0fe
Vlad <vladpiroz@gmail.com>
2025-12-18 03:07:07 +0700

    Добавил gitignore

---
+++ Assets/Scripts/Killbox_Triger.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class TestKillbox : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log($"В триггер вошел: {other.name}");
+        
+        if (other.name.Contains("ball") || other.name.Contains("Ball"))
+        {
+            Debug.Log("МЯЧ УПАЛ! УНИЧТОЖАЕМ...");
+            Destroy(other.gameObject);
+        }
+    }
+}
